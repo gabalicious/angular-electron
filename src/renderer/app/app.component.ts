74058e4d09557cf8ab5fdc6642f13df57e3dba77
gabalicious <gabalicious113@gmail.com>
2018-01-29 01:15:03 -0500

    Xterm working, Socket not

--- src/renderer/app/app.component.ts
+++ src/renderer/app/app.component.ts
@@ -1,9 +1,18 @@
 import {Component, ViewEncapsulation} from "@angular/core";
-import "./global-styles.scss"
-import * as mcss from "./app.mcss"
-import { spawn, exec } from 'child_process';
-const sh = require("shelljs");
-const path = require('path')
+import "xterm/dist/xterm.css"
+import "xterm/lib/addons/fullscreen/fullscreen.css"
+import { Terminal } from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
+import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen';
+import * as attach from 'xterm/lib/addons/attach/attach';
+import * as terminado from 'xterm/lib/addons/terminado/terminado';
+
+// console.log(fullscreen, fit)
+
+Terminal.applyAddon(fullscreen);
+Terminal.applyAddon(attach);  // Apply the `attach` addon
+Terminal.applyAddon(fit);  // Apply the `fit` addon
+Terminal.applyAddon(terminado);  // Apply the `terminado` addon
 
 @Component({
   selector: '#app',
@@ -12,79 +21,47 @@ const path = require('path')
 })
 export class AppComponent {
   constructor(){
-    this.mcss = mcss;
-    this.alwaysFocusInput();
-    this.output = '';
-    this.history = [];
-    this.currentDir = '/Users/justingaba/development/';
-    // this.currentDir = path.resolve(process.cwd(), '..');
+    this.input = '';
+  }
+  ngOnInit() {
+
+    this.term = new Terminal();
+    var socketURL = 'ws://localhost:8010/websocket';
+    console.log(socketURL)
+    var socket = new WebSocket(socketURL);
+    socket.onmessage = function(e){ console.log(e.data); };
+    socket.onopen = () => conn.send('hello');
+    this.container = document.getElementById('terminal-container');
+    this.term.open(this.container);
+
+
+
+    this.term.writeln('Welcome to xterm.js');
+    this.term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
+    this.term.writeln('Type some keys and commands to play around.');
+    this.term.writeln('');
+    this.term.attach(socket);  // Attach the above socket to `term`
+    this.term.toggleFullScreen(true);  // Now the terminal should occupy the full viewport
+    // this.term.fit();  // Make the terminal's size and geometry fit the size of #terminal-container
+
+    this.term.on('key',  (key, ev)=>{
+      console.log(key)
+      console.log(ev)
+
+      if (ev.key === 'Enter'){
+        this.term.writeln('');
+        this.input = ''
+        this.term.terminadoAttach(socket, true, true);
+
+      }else{
+        this.term.write(key); //Error here, not found the 'write' property, this.term is not recognized correctly.
+      }
+    });
+    this.term.on('lineFeed',  (key, ev)=>{
+      console.log(key)
+      console.log(ev)
+    });
 
   }
 
-  terminalExec(input) {
-    let parentDir =  this.currentDir ||  '/Users/justingaba/development/';
-    let writeHistory = this.writeHistory.bind(this);
-
-    let cmd = exec(`${input};pwd`, {
-      cwd: parentDir
-    });
-
-    console.log(input)
-    cmd.stdin.on('data', function (data) {
-
-      writeHistory(data.split(/\n/));
-      console.log({input, status: 'in', output: data.split(/\n/)})
-    });
-
-    cmd.stdout.on('data', function (data) {
-      writeHistory(data.split(/\n/));
-      console.log({input, status: 'out', output: data.split(/\n/)})
-    });
-
-    cmd.stderr.on('data', function (data) {
-      writeHistory(data.split(/\n/));
-      console.log({input, status: 'err', output: data.split(/\n/)})
-    });
-
-    cmd.on('close', function (code) {
-      console.log('exit')
-
-      });
-      // console.log(this.history)
-
-    }
-    onSubmit(f: NgForm){
-      this.writeHistory(this.userInput)
-      this.terminalExec(this.userInput);
-      this.userInput = '';
-    }
-    alwaysFocusInput(){
-      setInterval(function(){
-        var focusbox;
-        focusbox = document.getElementById("input");
-        focusbox.focus();
-      })
-    }
-
-    getHistory(){
-      return this.history;
-    }
-    writeHistory(item){
-      console.log(item)
-      if(typeof item === "string"){
-        this.history.push(item);
-
-      }
-      else if(Array.isArray(item)){
-        // last item is pwd; make this currentDir
-
-        this.currentDir = item[item.length-2]
-        console.log(  this.currentDir )
-        console.log( item[item.length-2] )
-        item.forEach((i)=> {
-          this.history.push(i);
-        });
-      }
-    }
-
-  }
+}
